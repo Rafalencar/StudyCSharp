c8a88e88bdbf96a938d918659db5c650ed9be766
Rafael de Alencar <rafalencar@hotmail.com>
2016-08-09 17:48:33 -0300

    Initial Commit

---
+++ Study.LojaVirtual.UnitTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Study.LojaVirtual.UnitTest
+{
+    [TestClass]
+    public class UnitTestStudy
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
